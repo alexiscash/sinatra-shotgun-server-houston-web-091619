a91fb04859640eecd56e1334eb0bb17e6c529a01
alexiscash <alexiscash97@rocketmail.com>
2019-10-10 09:38:26 -0500

    done

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Started this bitch up using mf SHOTGUN blat blat"
+    "Started this bitch up using mf SHOTGUN blat blatt"
   end
 
 end
